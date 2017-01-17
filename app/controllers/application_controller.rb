5c1d7fd3469286f36de6809b9bdf3e98cf4f7329
Valentun <karbon4ic@gmail.com>
2017-01-17 21:13:45 +0600

    fix Authorized users only message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_session
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :authenticate_user!
+	before_action :authenticate_user!, only: [:show_cookbook]
 
 
 	def show_cookbook
